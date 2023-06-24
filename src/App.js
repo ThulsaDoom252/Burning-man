213700cc0889b20356605987842f58b567daf854
ThulsaDoom252 <xenolm252@gmail.com>
2023-06-24 17:01:29 +0300

    Implement Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import 'reset-css'
 import Header from "./components/Header/Header";
 import Section from "./components/Section/Section";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
     return <div className={'wrapper'}>
         <Header/>
         <Section/>
-
+        <Footer/>
     </div>
 
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className={'footer'}>
+            <div className={'container'}>
+                <span className={'footer-text-1 footer-text'}>©1989-2020 burning man project</span>
+                <span className={'footer-text-2 footer-text'}>WAYUP & Андрей Гаврилов</span>
+                <span className={'footer-text-3 footer-text'}>Layout by ThulsaDoom</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
